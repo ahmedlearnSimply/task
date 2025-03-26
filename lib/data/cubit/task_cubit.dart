cc135c56b4e4ed18a550027425a99caa6b3bb88c
ahmed adel <ahmedadel123422@gmail.com>
2025-03-27 01:51:46 +0200

    commit

---
+++ lib/data/cubit/task_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task/data/cubit/task_states.dart';
+import 'package:task/data/model/taskModel.dart';
+import 'package:uuid/uuid.dart';
+
+class TaskCubit extends Cubit<TaskState> {
+  TaskCubit() : super(TaskInitialState());
+
+  //! Have Three Functions
+
+  addTask(String taskName) {
+    final model = TaskModel(
+      id: Uuid().v1(),
+      title: taskName,
+      isCompleted: false,
+    );
+    emit(UpdateTaskState([...state.taskList, model]));
+  }
+
+  deleteTask(int id) {
+    final List<TaskModel> newList =
+        state.taskList.where((task) => task.id != id).toList();
+    emit(UpdateTaskState(newList));
+  }
+
+  toggleTask(int id) {
+    final List<TaskModel> newList = state.taskList.map((task) {
+      return task.id == id
+          ? task.coptWith(isCompleted: !task.isCompleted)
+          : task;
+    }).toList();
+    emit(UpdateTaskState(newList));
+  }
+}
---
+++ lib/data/presentation/HomePage.dart
@@ -0,0 +1,101 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
+import 'package:task/data/cubit/task_cubit.dart';
+import 'package:task/data/cubit/task_states.dart';
+
+class Homepage extends StatelessWidget {
+  Homepage({super.key});
+  TextEditingController taskController = new TextEditingController();
+  GlobalKey formKey = new GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 249, 243, 243),
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text(
+          "Taskiti",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: BlocProvider(
+        create: (context) => TaskCubit(),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: taskController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      labelText: 'Task Title',
+                      hintText: 'Enter a task title',
+                    ),
+                  ),
+                  Gap(20),
+                  BlocBuilder<TaskCubit, TaskState>(
+                    builder: (context, state) {
+                      return ElevatedButton(
+                        onPressed: () {
+                          context.read<TaskCubit>().addTask(
+                                taskController.text,
+                              );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Colors.blueAccent, // Button background color
+                          foregroundColor: Colors.white, // Text color
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 24,
+                            vertical: 12,
+                          ), // Button padding
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(12), // Rounded corners
+                          ),
+                          elevation: 5, // Shadow elevation
+                        ),
+                        child: Text(
+                          "Add Task",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  Gap(20),
+                  ListTile(
+                    title: Text(taskController.text),
+                    leading: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.check_box_outline_blank_outlined,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
